0deb99a7ac4770dcea31aa3bf94f0a6000958c99
thamuz24 <nguyenhoanggiaminh24@gmail.com>
2023-08-07 09:04:14 +0700

    jdbc

---
+++ src/ConnectionJDBC.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionJDBC {
+    private String hostName = "localhost:3306";
+    private String dbName = "quanlydiem";
+    private String username = "root";
+    private String password = "1";
+
+    private String connectionURL = "jdbc:mysql://"+hostName+"/"+dbName;
+
+    public Connection connect(){
+        //Tạo đối tượng Connection
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(connectionURL, username, password);
+            System.out.println("Kết nối thành công");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+
+}
