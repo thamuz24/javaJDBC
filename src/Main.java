8b4bb8b536275d2528b39b221da8965428764525
thamuz24 <nguyenhoanggiaminh24@gmail.com>
2023-08-07 09:30:22 +0700

    th query

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
         ConnectionJDBC connectJDBC = new ConnectionJDBC();
-        connectJDBC.connect();
+        Connection conn = connectJDBC.connect();
+        String query = "SELECT * FROM KETQUA";
+
+        Statement stm = null;
+        try {
+            stm = conn.createStatement();
+
+            ResultSet rs = stm.executeQuery(query);
+
+            while (rs.next()){  //Di chuyển con trỏ xuống bản ghi kế tiếp
+                String masv = rs.getString("masv");
+                String mahm = rs.getString("mamh");
+                int lanthi = rs.getInt("lanthi");
+                double diemthi = rs.getDouble("diemthi");
+                System.out.println(masv + " - " + mahm + " - " + lanthi + " - " + diemthi);
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
